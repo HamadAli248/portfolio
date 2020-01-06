bb7c5d5626b251d1d24fa7d3927682a624bbbfe1
hammadali248 <hammadali248@gamil.com>
2020-01-06 14:49:33 +0000

    Updates

---
+++ precache-manifest.a6be5fb6ee322cb25068de7ffe0c4230.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e053ac319525f25b29b2fd0c27714df4",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "d64db1569378cde6818a",
+    "url": "/react-portfolio/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "ea93d6f7dd4550dcc33c",
+    "url": "/react-portfolio/static/css/main.8f1ccf07.chunk.css"
+  },
+  {
+    "revision": "d64db1569378cde6818a",
+    "url": "/react-portfolio/static/js/2.627e8631.chunk.js"
+  },
+  {
+    "revision": "ea93d6f7dd4550dcc33c",
+    "url": "/react-portfolio/static/js/main.cdf3dde2.chunk.js"
+  },
+  {
+    "revision": "52673c9149f008ba6357",
+    "url": "/react-portfolio/static/js/runtime-main.6301be62.js"
+  },
+  {
+    "revision": "d4af090ad62fc5045588628c687479a1",
+    "url": "/react-portfolio/static/media/GBuster.d4af090a.jpg"
+  },
+  {
+    "revision": "dda9db5347b9d9ff9ee6a4fe18023eb7",
+    "url": "/react-portfolio/static/media/HamadAliCV.dda9db53.pdf"
+  }
+]);
