e1807e04ca0013e7632c68f76ebcf51c8ca72fd8
Hamad Ali <hamad.ali@dmwgroup.co.uk>
2020-04-01 22:09:29 +0100

    added drawer tab to media query for project page

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import { Tabs, Tab , Grid , Cell } from 'react-mdl';
-
+import { Tabs, Tab , Grid , Cell,Drawer,Layout,Content} from 'react-mdl';
 import { ReactProjects } from './projectLanuages/ReactProjects';
 import { VanillaJavaScript } from './projectLanuages/VanillaJavaScript';
 import { BasicChallenges } from './projectLanuages/BasicChallenges';
 import { BasicChallengesInJava } from './projectLanuages/BasicChallangesInJava';
 import {JavaProjects } from './projectLanuages/JavaProjects';
 import { MongoDb } from './projectLanuages/MongoDB';
-
+import { Link } from 'react-router-dom';
 
 class Projects extends Component {
     constructor(props){
@@ -16,46 +15,35 @@ class Projects extends Component {
     }
        
     toggleCategories(){
-
-        
             if(this.state.activeTab === 0){
                 return(
-                    <div>
+                    <div className="all-projects">
                         <ReactProjects />
                         <JavaProjects/>
-                        <MongoDb/>
                         <VanillaJavaScript />
-                        
+                        <MongoDb/>
                     </div>
-                    
                 )
             }else if(this.state.activeTab === 1){
                 return(
-                <JavaProjects/>
-              
+                <JavaProjects/>              
                 )     
             }else if(this.state.activeTab === 2){
                 return(
                     < ReactProjects />
-                
                 )
-                
             }else if(this.state.activeTab === 3){
                 return(
                     <MongoDb/>
-                
                 )
-                
             }
             else if(this.state.activeTab === 4){
                 return(
                     <VanillaJavaScript />
-                
                 )
             }else if(this.state.activeTab === 5){
                 return(
-                    <BasicChallenges />
-               
+                    <BasicChallenges />               
                 )
             }
             else if(this.state.activeTab === 6){
@@ -64,28 +52,69 @@ class Projects extends Component {
                 )
             }
     }
+   
     
     render() {
+        
         return(
-            <div className="category-tabs">
-                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab:tabId})} ripple>
-                <Tab>All Projects</Tab>
-                <Tab>Java</Tab>
-                <Tab>React</Tab>
-                <Tab>MongoDB</Tab>
-                <Tab>Vanilla Javascript</Tab>
-                <Tab>Basic Challenges Javascript</Tab>
-                <Tab>Basic Challenges Java</Tab>
-                
-                </Tabs>
+            <div>
+                <div className="category-tabs">
+                    <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab:tabId})} ripple>
+                    <Tab>All Projects</Tab>
+                    <Tab>Java</Tab>
+                    <Tab>React</Tab>
+                    <Tab>MongoDB</Tab>
+                    <Tab>Vanilla Javascript</Tab>
+                    <Tab>Basic Challenges Javascript</Tab>
+                    <Tab>Basic Challenges Java</Tab>
+                    </Tabs>
+                        <Grid >
+                            <Cell col={12}>
+                                <div className="content">{this.toggleCategories()}</div>
+                            </Cell>
+                        </Grid>
+                </div>
+                <div className="category-tabs-select">
+                <Layout>
+                        <Drawer >
+                            <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab:tabId})} ripple>
+                                <Tab>All Projects</Tab>
+                                <Tab>Java</Tab>
+                                <Tab>React</Tab>
+                                <Tab>MongoDB</Tab>
+                                <Tab>Vanilla Javascript</Tab>
+                                <Tab>Basic Challenges Javascript</Tab>
+                                <Tab>Basic Challenges Java</Tab>
+                            </Tabs>
+                        </Drawer>
+                        <Content>
+                            <Grid >
+                                <Cell col={12}>
+                                    <div className="content">{this.toggleCategories()}</div>
+                                </Cell>
+                            </Grid>
+                        </Content>
+                        </Layout>
+              </div>
+                {/* <div className="category-tabs-select">
+                    <select activeoption={this.state.activeoption} onChange={(optionId) => this.setState({activeoption:optionId})}>
+                        <option>All Projects</option>
+                        <option>Java</option>
+                        <option>React</option>
+                        <option>MongoDB</option>
+                        <option>Vanilla Javascript</option>
+                        <option>Basic Challenges Javascript</option>
+                        <option>Basic Challenges Java</option> 
+                    </select>
                     <Grid >
-                        <Cell col={12}>
-                            <div className="content">{this.toggleCategories()}</div>
-                        </Cell>
-                    </Grid>
+                            <Cell col={12}>
+                                <div className="content">{this.toggleCategories()}</div>
+                            </Cell>
+                        </Grid>
+                </div> */}
             </div>
-        )
-    }
+            )
+    }        
 }
 export default Projects;
 
