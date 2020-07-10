e429d628e911aafedff30e42923a688996334639
Hamad Ali <hammadali248@gmail.com>
2020-07-10 14:46:59 +0100

    Updates

---
+++ precache-manifest.564c23d0dfc481935e01e2f2c24297de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c4a6ffaec0ad7e7757597683756b3b9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9c9084567e7d97d772d6",
+    "url": "/portfolio/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "e6fa873d3bc5f92b2792",
+    "url": "/portfolio/static/css/main.05284cec.chunk.css"
+  },
+  {
+    "revision": "9c9084567e7d97d772d6",
+    "url": "/portfolio/static/js/2.14206dd9.chunk.js"
+  },
+  {
+    "revision": "e6fa873d3bc5f92b2792",
+    "url": "/portfolio/static/js/main.41de4116.chunk.js"
+  },
+  {
+    "revision": "aa943007cc905238403d",
+    "url": "/portfolio/static/js/runtime-main.3fe3d468.js"
+  },
+  {
+    "revision": "d4af090ad62fc5045588628c687479a1",
+    "url": "/portfolio/static/media/GBuster.d4af090a.jpg"
+  },
+  {
+    "revision": "dda9db5347b9d9ff9ee6a4fe18023eb7",
+    "url": "/portfolio/static/media/HamadAliCV.dda9db53.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio/precache-manifest.cb15704f89fe394f5bfe730021c1d241.js"
+  "/portfolio/precache-manifest.564c23d0dfc481935e01e2f2c24297de.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
