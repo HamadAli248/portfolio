497b193de8bb247f28c5e3bfcfc3954482365919
Hamad Ali <hammadali248@gmail.com>
2020-07-10 13:41:36 +0100

    worked on media query on project page tab bar

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,69 +1,110 @@
-import React, { Component } from 'react';
-import { Tabs, Tab , Grid , Cell} from 'react-mdl';
+import React from 'react';
 import { ReactProjects } from './projectLanuages/ReactProjects';
 import { VanillaJavaScript } from './projectLanuages/VanillaJavaScript';
 import { BasicChallenges } from './projectLanuages/BasicChallenges';
 import {JavaProjects } from './projectLanuages/JavaProjects';
 import { MongoDb } from './projectLanuages/MongoDB';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
-class Projects extends Component {
-    constructor(props){
-        super(props);
-        this.state = { activeTab: 0 };
-    }     
-    toggleCategories(){
-            if(this.state.activeTab === 0){
-                return(
-                    <div className="all-projects">
-                        <ReactProjects />
-                        <JavaProjects/>
-                        <VanillaJavaScript />
-                        <MongoDb/>
-                    </div>
-                )
-            }else if(this.state.activeTab === 1){
-                return(
-                <JavaProjects/>              
-                )     
-            }else if(this.state.activeTab === 2){
-                return(
-                    < ReactProjects />
-                )
-            }else if(this.state.activeTab === 3){
-                return(
-                    <MongoDb/>
-                )
-            }
-            else if(this.state.activeTab === 4){
-                return(
-                    <VanillaJavaScript />
-                )
-            }else if(this.state.activeTab === 5){
-                return(
-                    <BasicChallenges />                                  
-                )
-            }
-    }
-    render() {
-        return(
-                <div className="category-tabs">
-                    <div id="tabs">
-                        <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab:tabId})} ripple>  
-                            <Tab id="text" >All Projects</Tab>
-                            <Tab id="text">Java</Tab>
-                            <Tab id="text">React</Tab>
-                            <Tab id="text">MongoDB</Tab>
-                            <Tab id="text">Vanilla Javascript</Tab>
-                            <Tab id="text">Basic Challenges </Tab>
-                        </Tabs>
-                    </div>
-                        <Grid >
-                            <Cell col={12}>
-                                <div className="content">{this.toggleCategories()}</div>
-                            </Cell>
-                        </Grid>
-                </div>
-            )
-    }        
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`scrollable-auto-tabpanel-${index}`}
+      aria-labelledby={`scrollable-auto-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
 }
-export default Projects;
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `scrollable-auto-tab-${index}`,
+    'aria-controls': `scrollable-auto-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    width: '100%',
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function ScrollableTabsButtonAuto() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="auto" color="default">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="scrollable"
+          scrollButtons="auto"
+          aria-label="scrollable auto tabs example"
+        >
+          <Tab label="All Projects" {...a11yProps(0)} />
+          <Tab label="Java" {...a11yProps(1)} />
+          <Tab label="React" {...a11yProps(2)} />
+          <Tab label="MongoDB" {...a11yProps(3)} />
+          <Tab label="Vanilla Javascript" {...a11yProps(4)} />
+          <Tab label="Basic Challenges" {...a11yProps(5)} /> 
+        </Tabs>
+       
+      </AppBar>
+      <TabPanel value={value} index={0}>
+            <ReactProjects />
+            <JavaProjects/>
+            <VanillaJavaScript />
+            <MongoDb/>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+         <JavaProjects/>
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+          <ReactProjects />
+         
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+          <MongoDb/>
+        
+      </TabPanel>
+      <TabPanel value={value} index={4}>
+          <VanillaJavaScript />
+      </TabPanel>
+      <TabPanel value={value} index={5}>
+      <BasicChallenges />     
+      </TabPanel>
+    </div>
+  );
+}
