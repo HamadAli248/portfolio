52f1a9334c865374411d0f672fc4a7f475068cdf
HamadAli248 <hammadali248@gmail.com>
2020-08-06 11:36:57 +0000

    Updates

---
+++ precache-manifest.f760e477b5f679bdfbfb37ff5872c89a.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "856171002f39c1c1e6966f36ccd5b9f8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5a12e2ccbf8a85e09b33",
+    "url": "/portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "7d4cbbe79c21ee0cce1f",
+    "url": "/portfolio/static/css/main.25ecc1b3.chunk.css"
+  },
+  {
+    "revision": "5a12e2ccbf8a85e09b33",
+    "url": "/portfolio/static/js/2.e4cd5b47.chunk.js"
+  },
+  {
+    "revision": "7d4cbbe79c21ee0cce1f",
+    "url": "/portfolio/static/js/main.708bdf1a.chunk.js"
+  },
+  {
+    "revision": "063006be7bb8ee6e2e88",
+    "url": "/portfolio/static/js/runtime-main.e9ec26ce.js"
+  },
+  {
+    "revision": "d4af090ad62fc5045588628c687479a1",
+    "url": "/portfolio/static/media/GBuster.d4af090a.jpg"
+  },
+  {
+    "revision": "dda9db5347b9d9ff9ee6a4fe18023eb7",
+    "url": "/portfolio/static/media/HamadAliCV.dda9db53.pdf"
+  },
+  {
+    "revision": "53760031fa35db187855a2ae545fda0d",
+    "url": "/portfolio/static/media/urlBlog.53760031.png"
+  },
+  {
+    "revision": "fed3d91993272778540e0bd8f6ad320d",
+    "url": "/portfolio/static/media/urlBooking.fed3d919.png"
+  },
+  {
+    "revision": "4fc34b305c0e80c62fa07378f3cfd115",
+    "url": "/portfolio/static/media/urlBrickbreaker.4fc34b30.jpg"
+  },
+  {
+    "revision": "bde2fd9fd0fe43cf632c13e5cc69d972",
+    "url": "/portfolio/static/media/urlCalculator.bde2fd9f.png"
+  },
+  {
+    "revision": "b591dcb3477b3001be04172305e91960",
+    "url": "/portfolio/static/media/urlChallenge.b591dcb3.png"
+  },
+  {
+    "revision": "efb9f032cf5313675c1cc9c3c199eedc",
+    "url": "/portfolio/static/media/urlChatbox.efb9f032.png"
+  },
+  {
+    "revision": "74d805ba5d1aa09d353182387154ee26",
+    "url": "/portfolio/static/media/urlCodeEditor.74d805ba.webp"
+  },
+  {
+    "revision": "de3292e0d61a77ffeab675a1d80008c6",
+    "url": "/portfolio/static/media/urlDataBase.de3292e0.jpg"
+  },
+  {
+    "revision": "9d96169626eae1b45638b3c64d5dd3b9",
+    "url": "/portfolio/static/media/urlDom.9d961696.png"
+  },
+  {
+    "revision": "8885ce9860f3aed4c999dc1b9fb70633",
+    "url": "/portfolio/static/media/urlJavalogo.8885ce98.png"
+  },
+  {
+    "revision": "fbb1b58275e40dcba9c683cefe6cd6bb",
+    "url": "/portfolio/static/media/urlMongoDB.fbb1b582.png"
+  },
+  {
+    "revision": "883617ab702cfab9f79acff343043eea",
+    "url": "/portfolio/static/media/urlPortfolio.883617ab.jpg"
+  },
+  {
+    "revision": "3f065239d392d532efbfd5b33853e079",
+    "url": "/portfolio/static/media/urlTDD.3f065239.png"
+  },
+  {
+    "revision": "309e15513ff197f3bc31cc048514373f",
+    "url": "/portfolio/static/media/urlTetris.309e1551.png"
+  },
+  {
+    "revision": "4c0599264d7d053aa597c4ef0cf323e4",
+    "url": "/portfolio/static/media/urlTicketMacine.4c059926.png"
+  },
+  {
+    "revision": "d97cecdf36e60b071f5018aef6106a71",
+    "url": "/portfolio/static/media/urlWeather.d97cecdf.png"
+  },
+  {
+    "revision": "283ea2ed23627d02a797f82056944a5a",
+    "url": "/portfolio/static/media/urlZenGarden.283ea2ed.png"
+  },
+  {
+    "revision": "360e6efb490708f5c77f2e7e9918ece4",
+    "url": "/portfolio/static/media/urlinsta.360e6efb.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.790f222a1cf147d3d02341863d214b6f.js"
+  "/portfolio/precache-manifest.f760e477b5f679bdfbfb37ff5872c89a.js"
 );
 
 self.addEventListener('message', (event) => {
