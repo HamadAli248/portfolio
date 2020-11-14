e648d637d02abc671e8217cf4027188a75775b46
Hamad Ali <hammadali248@gmail.com>
2020-11-14 18:56:22 +0000

    testing github actions due to new personal token

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -88,14 +88,16 @@ class About extends Component {
               <h2>About Me</h2>
               <p>
                 Currently I am on a 15 months Apprenticeship at{" "}
-                <a href="https://www.dmwgroup.com/">DMW Group</a>. Completed the
-                first 12-week training at Code Nation, Manchester. Learning
-                Front-End development including HTML5, CSS3, JavaScript, React,
-                Back-End programming such as APIs, Node.JS, Mongo. DB, MySQL,
-                Express, iOS Swift programming and using the terminal and
-                command line,and using git hub. I've have also been learning
-                Java and looking into Databases(PostgreSQL),Spring Boot and
-                finally AWS.
+                <a href="https://www.dmwgroup.com/">
+                  DMW Group a Credera Company
+                </a>
+                . Completed the first 12-week training at Code Nation,
+                Manchester. Learning Front-End development including HTML5,
+                CSS3, JavaScript, React, Back-End programming such as APIs,
+                Node.JS, Mongo. DB, MySQL, Express, iOS Swift programming and
+                using the terminal and command line,and using git hub. I've have
+                also been learning Java and looking into
+                Databases(PostgreSQL),Spring Boot and finally AWS.
                 <br />
                 <br />I am a sporty person, I enjoy playing Cricket, Football
                 and going for a Run. I also enjoy watching Formula 1 and Boxing.
