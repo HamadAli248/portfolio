cd5f8b78e8862d6a596a895f78d5ec55196288dd
Hamad Ali <hammadali248@gmail.com>
2020-12-05 14:21:22 +0000

    resolved issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
-import Main from "./components/Main";
+import Main from "./components/main";
 import { Link } from "react-router-dom";
 import CV from "./HamadAliCV.pdf";
 import AboutMe from "./components/AboutMe";
