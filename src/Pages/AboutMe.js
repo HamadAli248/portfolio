91a5c255dd646496d0fff993977c82ce837e7f55
Hamad Ali <hammadali248@gmail.com>
2020-12-21 16:12:26 +0000

    removed commented out code

--- src/Pages/AboutMe.js
+++ src/Pages/AboutMe.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-// import { Grid, Cell } from "react-mdl";
 import { Certification } from "../components/AboutMePage/Certification/Certifications";
 import styles from "../components/AboutMePage/AboutMe/AboutMe.module.css";
 import Intro from "../components/AboutMePage/AboutMe/Intro";
@@ -11,18 +10,12 @@ class About extends Component {
   render() {
     return (
       <div className={styles.aboutMePage}>
-        {/* <Grid>
-          <Cell className={styles.mdlCell} col={4}> */}
         <Intro />
         <SocialLinks />
         <Skills />
-        {/* </Cell> */}
-        {/* <Cell className={styles.aboutMeRightCol} col={8}> */}
         <Bio />
         <hr style={{ borderTop: "3px solid #e22947" }} />
         <Certification />
-        {/* </Cell>
-        </Grid> */}
       </div>
     );
   }
