65cec9ebdb7c8a2bf0b07dce39c967a4c529f94a
Hamad Ali <hammadali248@gmail.com>
2020-12-21 11:02:07 +0000

    moved navbar out of app.js

--- src/App.js
+++ src/App.js
@@ -1,54 +1,14 @@
 import React from "react";
 import "./App.css";
-import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+import { Layout, Content } from "react-mdl";
 import Main from "./components/Main";
-import { Link } from "react-router-dom";
-import CV from "./Pages/HamadAliCV.pdf";
-import AboutMe from "./Pages/AboutMe.js";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="demo-big-content">
-      {AboutMe}
       <Layout>
-        <Header
-          className="header-color"
-          title={
-            <Link
-              style={{ textDecoration: "none", color: "white" }}
-              to="/portfolio/"
-            >
-              Hamad Ali
-            </Link>
-          }
-          scroll
-        >
-          <Navigation>
-            <Link to="/aboutme">About Me</Link>
-            <Link to="/projects">Projects</Link>
-            <Link to={CV} target="_blank">
-              Resume
-            </Link>
-          </Navigation>
-        </Header>
-        <Drawer
-          title={
-            <Link
-              style={{ textDecoration: "none", color: "black" }}
-              to="/portfolio/"
-            >
-              Hamad Ali
-            </Link>
-          }
-        >
-          <Navigation>
-            <Link to="/aboutme">About Me</Link>
-            <Link to="/projects">Projects</Link>
-            <Link to={CV} target="_blank">
-              Resume
-            </Link>
-          </Navigation>
-        </Drawer>
+        <Navbar />
         <Content>
           <div className="page-content"></div>
           <Main />
