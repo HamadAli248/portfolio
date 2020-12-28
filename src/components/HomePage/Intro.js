c290e89cae981f430749ce6342e6c9062a1c2e12
Hamad Ali <hammadali248@gmail.com>
2020-12-28 09:06:26 +0000

    added animation to the button to redirect to about me page

--- src/components/HomePage/Intro.js
+++ src/components/HomePage/Intro.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import styles from "./Intro.module.css";
-import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
 
 class Intro extends Component {
@@ -17,11 +16,11 @@ class Intro extends Component {
           <h1> I'm Hamad Ali. </h1>
           <h1> I am a Junior Software Engineer </h1>
           <div className={styles.buttonDiv}>
-            <Button variant="outlined" color="inherit" size="medium">
+            <button className={styles.btn}>
               <Link className={styles.link} to="/aboutme">
                 About Me
               </Link>
-            </Button>
+            </button>
           </div>
         </div>
       </div>
