92d9df2cb580801be5dbb82525f8c6a30bc77214
Hamad Ali <hammadali248@gmail.com>
2021-01-01 09:31:17 +0000

    set the input width

--- src/components/ContactMePage/ContactForm/ContactMeForm.js
+++ src/components/ContactMePage/ContactForm/ContactMeForm.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import emailjs from "emailjs-com";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import styles from "./ContactMeForm.module.css";
 
 class ContactMeForm extends Component {
   sendEmail(e) {
@@ -25,11 +26,17 @@ class ContactMeForm extends Component {
 
   render() {
     return (
-      <div style={{ textAlign: "center" }}>
-        <h1 style={{ color: "black" }}>Contact me</h1>
-        <form noValidate autoComplete="off" onSubmit={this.sendEmail}>
+      <div className={styles.formDiv}>
+        <h1>Contact me</h1>
+        <p>Have A question or want to work together?</p>
+        <form
+          className={styles.form}
+          noValidate
+          autoComplete="off"
+          onSubmit={this.sendEmail}
+        >
           <TextField
-            style={{ margin: "10px", width: "80%" }}
+            style={{ margin: "10px", width: "100%" }}
             id="outlined-basic"
             label="Name"
             type="text"
@@ -37,7 +44,7 @@ class ContactMeForm extends Component {
             variant="outlined"
           />
           <TextField
-            style={{ margin: "10px", width: "80%" }}
+            style={{ margin: "10px", width: "100%" }}
             id="outlined-basic"
             type="email"
             name="user_email"
@@ -45,17 +52,21 @@ class ContactMeForm extends Component {
             variant="outlined"
           />
           <TextField
-            style={{ margin: "10px", width: "80%" }}
+            style={{ margin: "10px", width: "100%" }}
             id="outlined-multiline-static"
             multiline
-            rows={4}
+            rows={5}
             name="message"
             label="Message"
             variant="outlined"
           />
           <Button
-            style={{ margin: "10px", width: "80%" }}
-            color="primary"
+            style={{
+              margin: "0 auto",
+              width: "4%",
+              height: "2%",
+            }}
+            color="none"
             variant="contained"
             type="submit"
             value="graph"
