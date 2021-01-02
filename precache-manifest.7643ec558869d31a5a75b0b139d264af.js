dd4c2d835340b8a41a80ad38fdad9b588a1b288b
HamadAli248 <hammadali248@gmail.com>
2021-01-02 11:18:24 +0000

    Updates

---
+++ precache-manifest.7643ec558869d31a5a75b0b139d264af.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9e1f9a0db7445d1a04f8c7376921653",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0f1df411affeacf83d3",
+    "url": "/static/css/2.bfa242bf.chunk.css"
+  },
+  {
+    "revision": "f68863921754fc1a8c4c",
+    "url": "/static/css/main.3f2eb924.chunk.css"
+  },
+  {
+    "revision": "a0f1df411affeacf83d3",
+    "url": "/static/js/2.74e1c149.chunk.js"
+  },
+  {
+    "revision": "f68863921754fc1a8c4c",
+    "url": "/static/js/main.b1b6e82f.chunk.js"
+  },
+  {
+    "revision": "5836338254787374b01e",
+    "url": "/static/js/runtime-main.194a46a3.js"
+  },
+  {
+    "revision": "3a8ad87b287b39bfee5eb84bd09c97c5",
+    "url": "/static/media/AWS Certified Cloud Practitioner certificate.3a8ad87b.pdf"
+  },
+  {
+    "revision": "36d9202ef83baa96b7baa3ab8f900cd0",
+    "url": "/static/media/BCSLOGO.36d9202e.jpg"
+  },
+  {
+    "revision": "bde2fd9fd0fe43cf632c13e5cc69d972",
+    "url": "/static/media/Calculator.bde2fd9f.png"
+  },
+  {
+    "revision": "48a892e5d76bf282c5bfc825797af9e6",
+    "url": "/static/media/FreeCodeCamp.48a892e5.png"
+  },
+  {
+    "revision": "d4af090ad62fc5045588628c687479a1",
+    "url": "/static/media/GBuster.d4af090a.jpg"
+  },
+  {
+    "revision": "dda9db5347b9d9ff9ee6a4fe18023eb7",
+    "url": "/static/media/HamadAliCV.dda9db53.pdf"
+  },
+  {
+    "revision": "77c49338f87054c630655d41b46952f0",
+    "url": "/static/media/MicrosoftExam480.77c49338.png"
+  },
+  {
+    "revision": "309e15513ff197f3bc31cc048514373f",
+    "url": "/static/media/Tetris.309e1551.png"
+  },
+  {
+    "revision": "d97cecdf36e60b071f5018aef6106a71",
+    "url": "/static/media/Weather.d97cecdf.png"
+  },
+  {
+    "revision": "464c92ce8d79242498348f28bd4a7236",
+    "url": "/static/media/bcs-methodologies.464c92ce.pdf"
+  },
+  {
+    "revision": "7a39274e0593c193a942561e2c07d96d",
+    "url": "/static/media/booking.7a39274e.jpeg"
+  },
+  {
+    "revision": "49aa76a6f6c7a9bcb1f7866e9bdfddb7",
+    "url": "/static/media/more.49aa76a6.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a8c3ae065e38ae3b5ee86087771243cc.js"
+  "/precache-manifest.7643ec558869d31a5a75b0b139d264af.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
